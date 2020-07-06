1adfd6556f2261c4ec117ecbce85baa99d253846
Mike Lofgren <mikeylofgren@gmail.com>
2020-07-05 22:45:31 -0700

    add in a nested component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 // This is the first thing that gets run in the browser. It will load the component
 // IE look for bootstrap
 @NgModule({
-  declarations: [AppComponent, ServerComponent],
+  declarations: [AppComponent, ServerComponent, ServersComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
