9180809794008e6deb5026166c19442c6bdd89b1
Mike Lofgren <mikeylofgren@gmail.com>
2020-07-05 17:22:27 -0700

    wire up new component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+
 // This is the first thing that gets run in the browser. It will load the component
 // IE look for bootstrap
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ServerComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
