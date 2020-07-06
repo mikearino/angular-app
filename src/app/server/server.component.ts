bfb3c119c3f0cd2b789f68ca8a2eed8c645fcb18
Mike Lofgren <mikeylofgren@gmail.com>
2020-07-05 17:08:16 -0700

    add server compnent html file.

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,3 +1,9 @@
+import { Component } from '@angular/core';
+
 // @Component is a decorator
-@Component
+@Component({
+  // selector is how a tag, so that you can use it in HTML template
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+})
 export class ServerComponent {}
