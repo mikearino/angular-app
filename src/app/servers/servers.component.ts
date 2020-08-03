ed33b2368846137f0a6447db89510bc033db529f
Mike Lofgren <mikeylofgren@gmail.com>
2020-08-03 07:51:24 -0700

    add in data binding.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  // selector: '[app-servers]',
+  selector: `.app-servers`,
   // Only use three lines of code, if not then you will need an external file
   template: `<app-server></app-server> <app-server></app-server>`,
   styleUrls: ['./servers.component.css'],
