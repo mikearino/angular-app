947d7033013f2402e5f908c31d9caf5c04b1c41b
Mike Lofgren <mikeylofgren@gmail.com>
2020-09-01 19:30:10 -0700

    add setInterval to control component.

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,6 +5,13 @@ import { Component } from '@angular/core';
   // selector is how a tag, so that you can use it in HTML template
   selector: 'app-server',
   templateUrl: './server.component.html',
+  styles: [
+    `
+      .online {
+        color: white;
+      }
+    `,
+  ],
 })
 export class ServerComponent {
   serverId: number = 10;
